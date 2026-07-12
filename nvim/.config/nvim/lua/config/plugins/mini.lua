d8a02e9fe01566cb870e5e5daa7c6451823dc79d
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2026-07-12 23:13:14 +0200

    (nvim) Swap mini.jump with flash

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -11,11 +11,6 @@ return {
 
       require('mini.surround').setup()
 
-      require('mini.jump').setup({
-        mappings = { repeat_jump = ';' },
-      })
-      vim.keymap.set({ 'n', 'x', 'o' }, ',', function() MiniJump.jump(nil, not MiniJump.state.backward) end)
-
       require('mini.comment').setup()
 
       require('mini.tabline').setup()
